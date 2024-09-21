d42105482a9c822d27dd7fc482bbc908e68414ce
Anushka-24-DataScience <shilpirbs@gmail.com>
2024-09-22 00:14:31 +0530

    project folder and requirements file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Anushka Srivastava",
+    author_email="anushka.asthana1983@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
