035484f2819b7eb5ea18e0220d5aba05c9a2be05
Anushka-24-DataScience <shilpirbs@gmail.com>
2024-09-22 18:40:44 +0530

    data ingested

---
+++ hate_speech/constants/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from datetime import datetime
+
+# Common constants
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
+ZIP_FILE_NAME = 'C:\Hate_Speech_Recognition\data\dataset.zip'
+LABEL = 'label'
+TWEET = 'tweet'
+
+
+# Data ingestion constants
+DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
+DATA_INGESTION_IMBALANCE_DATA_DIR = "imbalanced_data.csv"
+DATA_INGESTION_RAW_DATA_DIR = "raw_data.csv"
